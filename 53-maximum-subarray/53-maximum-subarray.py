40017a5aff6487ddbb486b1245ad6d8cda422158
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-06-30 16:38:56 +0300

    Time: 5051 ms (5.01%), Space: 28.7 MB (9.66%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,128 +1,38 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
-        prefix= [0]
-        
-        for i in range(len(nums)):
-            if prefix[-1] < 0:
-                prefix.append(nums[i])
-            else:
-                prefix.append(nums[i] + prefix[-1])
-        return(max(prefix[1:]))
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         total = sum(nums)
-#         final = total
-#         l,r = 0,len(nums) -1
-        
-#         while l <= r:
-#             if nums[l] > nums[r]:
-#                 total -= nums[r]
-#                 r -= 1
-                
-#             elif nums[l] < nums[r]:
-#                 total -= nums[l]
-#                 l += 1
-#             elif nums[l] == nums[r]:
-#                 total -= nums[l]
-                
-#                 if l < len(nums)-1 and r>0 and nums[l+1] >nums[r-1]:
-#                     r -= 1
-#                 else:
-#                     l +=1
-                
-#             if total > final:
-#                 final = total
-#         return (final)
-        
-        
+        """
+        0. if nums.length == 1:
+            return nums[0]
+        1. divide array in half:
+            1.2. get maxsubarray of left half
+            1.3. get maxsubarray of right half
+            1.4. get the central sum
+            1.5. return the maximum of the above three steps 
+        
+        """
+        if len(nums) == 1:
+            return nums[0]
+        
+        
+        middle = len(nums)//2
+        
+        left = self.maxSubArray(nums[:middle])
+        right= self.maxSubArray(nums[middle:])
+        
+        sum_r = -1000000000000
+        sum_l = -1000000000000
+        sum_ = 0
+        p = middle
+        
+        while p >= 0 :
+            sum_ += nums[p]
+            sum_r = max(sum_r, sum_)
+            p -= 1
+
+        p = middle + 1
+        sum_ = 0
+        while p < len(nums):
+            sum_ += nums[p]
+            sum_l = max(sum_, sum_l)
+            p += 1
+        return max(left, right, sum_r, sum_l, sum_r + sum_l)
