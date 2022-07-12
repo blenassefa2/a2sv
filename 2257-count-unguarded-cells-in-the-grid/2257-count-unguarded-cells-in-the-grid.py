287e5f2b760a988236dd4290c4b232c57c93c18c
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-07-12 18:22:04 +0300

    Time: 2789 ms (86.60%), Space: 42 MB (78.23%) - LeetHub

---
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -0,0 +1,64 @@
+class Solution:
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        """
+        brute force approach:
+        --------------------
+        
+        calculate total_space
+        construct the matrix
+        decreament the guards and wall from total_space
+        
+        for each guard 
+            calculate how long it can guard 
+            subtruct that from total_matrix
+            
+        return final of total_space
+        
+        complexity:
+        -----------
+        
+        Time complexity  = O(len(guards)*(n+m) + n*m)
+        space complexity = O(n*m)
+        
+        """
+        total_space = (n * m) - len(guards) - len(walls)
+        w , g = set(), set()
+        
+        for wall in walls:
+            w.add((wall[0], wall[1]))
+        for wall in guards:
+            g.add((wall[0], wall[1]))
+        found  = set()
+        
+        
+        for a,b in guards:
+            
+            
+            
+            for i in range(a+1, m):
+                
+                if (i,b) in w or (i,b) in g:
+                    break
+            
+                found.add((i,b))
+            for j in range(a-1,-1,-1):
+                
+                if (j,b) in w or (j,b) in g:
+                    break
+               
+                found.add((j,b))
+            for k in range(b+1, n):
+                
+                if (a,k) in w or (a,k) in g:
+                    break
+                
+                found.add((a,k))
+            for l in range(b-1,-1,-1):
+                
+                if (a,l) in w or (a,l) in g:
+                    break
+            
+                found.add((a,l))
+           
+        return (total_space) - len(found)
+        
