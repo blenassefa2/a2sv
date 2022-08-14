f832ac80150a305cd6df63af91aee6fd8e193464
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-08-14 16:09:17 +0300

    Time: 72 ms (70.83%), Space: 13.9 MB (74.15%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
@@ -1,38 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        l,r = 0,0
-       
-        
-        while r < len(nums):
-           
-            if 0<l and nums[l] < nums[l-1]:
-                break
-            while r< len(nums) and nums[r] == nums[l]:
-                # if nums[r] 
-                r += 1
-            
-            
-
-            count = 0
-            while l+1 < len(nums) and nums[l] ==nums[l+1] and count < 2:
-                count += 1
-                l += 1
-            
-            if count <2:
-                l +=1
-                continue
-             
-            if r < len(nums):    
-                if nums[l] > nums[r]:
-                    break
-                nums[l], nums[r] = nums[r], nums[l]
-                temp =l + 1
-                r += 1
-                while r < len(nums) and nums[temp] < nums[r]:
-                    nums[temp], nums[r] = nums[r], nums[temp]
-                    r += 1
-                    temp += 1
-               
-                r = l
-        
-        return l
+        i=j=0
+        if(len(nums)<3):
+            return len(nums);
+        s=2
+        for f in range(2,len(nums)):
+            if nums[f]!=nums[s-2]:
+                nums[s]=nums[f]
+                s+=1
+                
+        return s
