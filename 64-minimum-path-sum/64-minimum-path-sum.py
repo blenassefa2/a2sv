b41148f3bd2cfe5eefe1aa9665f4d02deb1a3963
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-08-14 09:33:13 +0300

    Time: 95 ms (98.61%), Space: 15.7 MB (84.71%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,18 +1,11 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        in_bound = lambda x,y: 0<=x<len(grid) and 0<=y<len(grid[0])
-        Dir =[[1,0], [0,1]]
-        @lru_cache()
-        def recurs(row, col):
-            if not in_bound(row,col):
-                return 10000000000
-            if row == len(grid)-1 and col == len(grid[0]) - 1:
-                return grid[-1][-1]
-            ans = 100000000000
-            for d in Dir:
-                n_r , n_c = row + d[0], col + d[1]
-                
-                ans = min(recurs(n_r,n_c), ans)
-                
-            return ans + grid[row][col]
-        return recurs(0,0)
+        for i in range(1,len(grid[0])):
+            grid[0][i] += grid[0][i-1]
+        for i in range(1, len(grid)):
+            grid[i][0] += grid[i-1][0]
+            
+        for i in range(1,len(grid)):
+            for j in range(1, len(grid[0])):
+                grid[i][j] += min(grid[i-1][j], grid[i][j - 1])
+        return grid[-1][-1]
