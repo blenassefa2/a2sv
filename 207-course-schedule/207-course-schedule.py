b19557ad8707b52e95345e1473fd54a0d10732a9
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-08-31 12:12:04 +0300

    Time: 1808 ms (5.00%), Space: 17.8 MB (5.18%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,26 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        inDegrees = [0]*numCourses
-        outDegrees = defaultdict(list)
-        qu = deque([])
+        visited = [0]*numCourses
+        outDegrees = defaultdict(set)
         
-        for i, o in prerequisites:
-            inDegrees[o] += 1
-            outDegrees[i].append(o)
+        for i,o in prerequisites:
+            outDegrees[i].add(o)
             
+        def recurs(node, dp):
+            if node in dp:
+                return dp[node]
+
+            if visited[node] == 1:
+                dp[node] = False
+                return False
+            
+            ans = True
+            visited[node] = 1
+            for n in outDegrees[node]:
+                ans = ans and recurs(n,dp)
+            
+            dp[node] = ans
+            visited[node] = 0
+            return ans
+            
+            
+        
+        """Driver code"""
+        
         for i in range(numCourses):
-            if inDegrees[i] == 0:
-                qu.append(i)
-        total = 0       
-        while qu:
-            n = qu.popleft()
-            total += 1
-            for i in outDegrees[n]:
-                inDegrees[i] -= 1
-                
-                if not inDegrees[i]:
-                    qu.append(i)
-                    
-        return total == numCourses
-        
-        
+            if visited[i] == 0:
+                if not recurs(i,defaultdict()):
+                    return False
+        return True
