42721328115475bebf172b2bcbd06f9a96caca73
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-09-18 00:32:26 +0300

    Time: 394 ms (11.49%), Space: 15.5 MB (85.59%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,15 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        if len(nums) == 1:
-            return nums[0]
-        winner = [nums[0], 1]
         
-        for i in range(1, len(nums)):
-            if winner[0] == nums[i]:
-                winner[1] += 1
-                if winner[1] > floor(len(nums) /2):
-                    return winner[0]
-            else:
-                winner = [nums[i], 1]
+        winner = [None, 0]
+        
+        for i in range(len(nums)):
+            if not winner[1]:
+                winner[0] = nums[i]
+                
+            winner[1] += (1 if nums[i] == winner[0] else -1)
+        return winner[0]
         
