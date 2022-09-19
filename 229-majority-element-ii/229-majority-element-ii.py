45e28382b82a55e6fa87b06ea9e850dc05a093eb
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-09-19 10:42:22 +0300

    Time: 348 ms (5.05%), Space: 15.5 MB (19.25%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -1,21 +1,33 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        nums.sort()
         
         
         n, ans = len(nums), set()
-        if n < 3:
-            return set(nums)
-        winner = [nums[0], 1]
+    
+        winner = [None, None]
+        w_count = [0,0]
         
-        for i in range(1,n):
+        for i in range(n):
             
-            if winner[0] == nums[i]:
-                winner[1] += 1
-                if winner[1] > floor(n/3):
-                    ans.add(winner[0])
+            if nums[i] == winner[0]:
+                w_count[0] += 1
+            elif nums[i] == winner[1]:
+                w_count[1] += 1
+            elif not w_count[0]:
+                winner[0] = nums[i]
+                w_count[0] += 1
+            elif not w_count[1]:
+                winner[1] = nums[i]
+                w_count[1] += 1
             else:
-                winner = [nums[i], 1]
+                w_count[0] -= 1
+                w_count[1] -= 1
         
+        if nums.count(winner[0]) > floor(n/3):
+            ans.add(winner[0])
+        if nums.count(winner[1]) > floor(n/3):
+            ans.add(winner[1])
+       
+            
         return ans
         
