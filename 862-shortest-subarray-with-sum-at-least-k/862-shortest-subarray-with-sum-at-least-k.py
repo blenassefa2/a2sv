3509880ade0a5cd91317307c216da1089160f7fb
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-09-22 15:57:21 +0300

    Time: 2354 ms (38.26%), Space: 28.3 MB (91.97%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.py
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.py
@@ -1,28 +1,24 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        """
-        1. have prefix array
-        2. do a sliding window on the prefix array
-        
-        """
         pre = [0]
-        mnlen = len(nums) + 1
-        qu = deque()
         
         for i in nums:
             pre.append(pre[-1] + i)
             
-        for i, n in enumerate(pre):
+        final = len(nums) +1
+        i = 0
+        qu = deque([])
+        while i < len(pre):
             
-            # to make sure the qu is monotonically increasing
-            while qu and pre[qu[-1]] >= n:
+            
+            while qu and pre[i] <=pre[qu[-1]]:
                 qu.pop()
-                
-            # if we achieve our target we try to reduce window size
-            # to see if we can get shorter sub array
-            while qu and n - pre[qu[0]] >= k:
-                mnlen = min(mnlen, i - qu.popleft())
+            
+            while qu and pre[i] - pre[qu[0]] >= k:
+                final = min(final, i - qu[0])
+                qu.popleft()
             
             qu.append(i)
-        
-        return -1 if mnlen == len(nums) + 1 else mnlen
+            i += 1
+            
+        return final if final <= len(nums) else -1
