4e27ead34d64b3cc776297c2e2ace5fc40075f07
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-10-09 00:27:47 +0300

    Time: 56 ms (82.15%), Space: 16 MB (6.78%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -1,14 +1,50 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        max_length = 0
-        stck=[-1] # initialize with a start index
+        """
+        I am thinking of using stack + sliding window
+        so that I can keep the length of the valid parentheses substring
+        
+        
+        while building my stack there are three scenarios:
+            1, I will pop valid parentheses
+                increase curr_count by two
+                procede as you normally would
+            2, I will get an element that for sure makes the stack sofar invalid
+                I update final_count if final_count < curr_count
+                clean up the the stack and procede
+            3, I will get an element whose outcome can not be determined yet
+                append it to the stack as you normally would
+        
+        """
+    
+        stack = []
+        curr = 0
+        final = 0
         for i in range(len(s)):
-            if s[i] == '(':
-                stck.append(i)
+            
+            if s[i] == "(":
+                stack.append([s[i],i])
             else:
-                stck.pop()
-                if not stck: # if popped -1, add a new start index
-                    stck.append(i)
+                if stack and stack[-1][0] == "(":
+                    curr = (i - (stack.pop())[1] +1)
+                    if stack and stack[-1][0] == "h":
+                        curr += stack.pop()[1]
+                    stack.append(["h", curr])
+                    final = max(final, curr)
+                    
+                elif len(stack) > 1 and stack[-1][0] == "h":
+                    d = stack.pop()
+                    c = stack.pop()
+                    curr = i - c[1] + 1
+                    if stack and stack[-1][0] == "h":
+                        curr += stack.pop()[1]
+                    final = max(final, curr)
+                    stack.append(["h",curr])
+                    
                 else:
-                    max_length=max(max_length, i-stck[-1]) # update the length of the valid substring
-        return max_length
+                    stack = []
+                    curr = 0
+           
+        
+                
+        return final
