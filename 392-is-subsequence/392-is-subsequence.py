7506b0c245717b8dcf22b96bae9285a12011f95c
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-10-10 06:30:30 +0300

    Time: 78 ms (9.39%), Space: 14.4 MB (6.05%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,13 +1,22 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if len(s) >= len(t):
-            return s==t
-        # if not(s;)
-        i = 0
-        stack = ""
+        hsh = defaultdict(list)
         
-        for j in range(len(t)):
-            if i < len(s) and  s[i] == t[j]:
-                stack += t[j]
-                i+= 1
-        return stack == s
+        for i in range(len(t)):
+            hsh[t[i]].append(i)
+            
+        small = -1
+        
+        for a in s:
+            if a in hsh:
+                x = 0
+                while x < len(hsh[a]) and hsh[a][x] <= small:
+                    x += 1
+                else:
+                    if x >= len(hsh[a]) or hsh[a][x] <= small:
+                        return False
+                    
+                    small = hsh[a][x]
+            else:
+                return False
+        return True
