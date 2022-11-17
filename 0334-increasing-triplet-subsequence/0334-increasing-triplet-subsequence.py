5214cc6b1d2e52e9e9a1fbd4128b43bc854bc45e
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-11-17 20:48:45 +0300

    Time: 3824 ms (5.01%), Space: 29.2 MB (5.00%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -0,0 +1,32 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        
+        # initialize my variables
+        isValid = False
+        maximums = [nums[-1]] # [4]
+        stack = []
+
+        # Building my maximums
+        for index in range(len(nums)-2, -1, -1):
+            maximums.append(max(maximums[-1], nums[index]))
+
+        maximums.reverse()
+
+        # traverse check if I can have valid combinations
+        index = 0
+        while index < len(nums):
+
+            if not stack:
+                stack.append(index)
+                index += 1
+            else:
+                if nums[stack[-1]] >= nums[index]:
+                    stack.pop()
+                else:
+                    if maximums[index] > nums[index]:
+                        return True
+                    index += 1
+
+
+        # return final answer
+        return False
