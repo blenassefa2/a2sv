e5b52dead2ee99fee23fcec9c794369d3ba77d97
Blen <62964622+blenassefa2@users.noreply.github.com>
2023-01-07 08:52:47 +0300

    Time: 59 ms (64.46%), Space: 13.9 MB (18.16%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -0,0 +1,34 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        """ To have TC of O(1) I will use count sort"""
+        
+        # initialize the necessary variables
+        counts = [0]* (max(nums) + 1)
+        answer = []
+        index = 0
+        countsIndex = 0
+        
+       
+        # fill the counts array
+        for i in nums:
+            counts[i] += 1
+            
+        # update nums to its sorted version inplace
+        while countsIndex < len(counts):
+            if counts[countsIndex] > 0:
+                nums[index] = countsIndex
+                
+                # update the answer
+                if nums[index] == target:
+                    answer.append(index)
+                
+                #update the indecies
+                counts[countsIndex] -= 1
+                index += 1
+            
+            if counts[countsIndex] == 0:
+                countsIndex += 1
+     
+        return answer
+        
+        
