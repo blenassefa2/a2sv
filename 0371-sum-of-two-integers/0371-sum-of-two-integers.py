e015cf7e471e9e1587c2fe149b818053f304f7bc
Blen <62964622+blenassefa2@users.noreply.github.com>
2023-05-11 21:51:10 +0300

    Time: 38 ms (28.97%), Space: 16.2 MB (11.98%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -3,16 +3,17 @@ class Solution:
         
         max_int = 0x7D0
         mask = 0xfff
-        def add(a,b):
-            sum_, carry = 0,1
+        
+       
+        ans, carry = 0,1
+
+        while carry:
+            ans, carry = (a^b)& mask, ((a&b) <<1)& mask
+            a,b = ans, carry
             
-            while carry:
-                sum_, carry = (a^b)& mask, ((a&b) <<1)& mask
-                a,b = sum_, carry
-            return sum_
           
        
         
-        ans = add(a,b)
+     
        
         return (~(ans^mask))  if ans > max_int else ans
