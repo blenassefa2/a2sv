f97d32062ab76741d3c4fabc2e925da0f3743757
Blen <62964622+blenassefa2@users.noreply.github.com>
2023-05-24 20:44:20 +0300

    Time: 828 ms (18.56%), Space: 18.1 MB (10.17%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,32 +1,25 @@
 class Solution:
-	def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-		if grid[0][0] or grid[-1][-1]:
-			return -1
-
-		n = len(grid)
-		if n == 1:
-			return 1
-
-		queue = deque([(0, 0, 1)])
-
-		def allowed_dirs(i, j):
-			output = [] 
-			for x in range(i + 1, i - 2, -1): #why not?
-				for y in range(j + 1, j - 2, -1):
-					if 0 <= x < n and 0 <= y < n and not grid[x][y]:
-						output.append((x, y))
-			return output
-
-		grid[0][0] = 1
-
-		while queue:
-			i, j, cur = queue.popleft()
-			cur += 1
-			for x, y in allowed_dirs(i, j):
-				grid[x][y] = 1 #there is no way back\U0001f642
-				if x == y == n - 1:
-#we do not need to compare with other paths because we move between levels with the same number of visited cells:
-					return cur
-				else:
-					queue.append((x, y, cur))
-		return -1
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+     
+        visited = set()
+        inbound = lambda x: 0 <= x[0]< len(grid) and 0<=x[1] < len(grid)
+        Dir = [[1,0],[0,1],[-1,0],[0,-1],[1,1],[-1,-1],[-1,1],[1,-1]]
+        queue = deque([[[0,0], 0]]) #we are starting from top corner
+        
+        while queue:
+            
+            [currX, currY], length = queue.popleft()
+            if (currX, currY) in visited:
+                continue
+            if grid[currX][currY] == 1:
+                continue
+            
+            if currX == currY == len(grid)-1:
+                return length + 1
+            visited.add((currX,currY))
+            for dx,dy in Dir:
+                newX, newY = dx + currX, dy +currY
+                
+                if (newX,newY) not in visited and inbound((newX, newY)) and grid[newX][newY]==0:
+                    queue.append([[newX,newY], length + 1])
+        return -1
